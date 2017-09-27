1888d5b4543e0a3f626d93f0ceb11eaac3d90500
Timothy Sullivan <tsullivan@elastic.co>
2017-09-27 14:07:55 -0700

    31 seconds

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const proxy = httpProxy.createProxyServer();
 
 const LISTEN_PORT = 9260;
 const TARGET_URL = 'http://localhost:9210';
+const TIMEOUT_TIME = 31 * 1000; // 31 seconds
 
 // A server that makes an operation that waits a while and then proxies the request
 http.createServer((req, res) => {
   // This simulates an operation that takes 500ms to execute
   setTimeout(() => {
     proxy.web(req, res, { target: TARGET_URL });
-  }, 1500);
+  }, TIMEOUT_TIME);
 }).listen(LISTEN_PORT);
 
 console.log(`Listening on ${LISTEN_PORT}, proxying to ${TARGET_URL}`);
