178c3fb720583e498cae4bc4bf41b8bedf071cf3
Timothy Sullivan <tsullivan@elastic.co>
2018-02-08 22:46:18 -0700

    https => https

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 /*eslint-disable no-console*/
-const http = require('http');
+const fs = require('fs');
+const https = require('https');
 const httpProxy = require('http-proxy');
 
-// Create a proxy server with latency
-const proxy = httpProxy.createProxyServer();
+const LISTEN_PORT = 9290;
+const TARGET_URL = 'https://spicy.local:9200';
+const TIMEOUT_TIME = 3 * 1000; // 3 seconds
 
-const LISTEN_PORT = 9260;
-const TARGET_URL = 'http://localhost:9200';
-const TIMEOUT_TIME = 5 * 1000; // 5 seconds
+const sslOpts = {
+  key: fs.readFileSync('./conf/spicy.local/spicy.local-key.pem', 'utf8'),
+  cert: fs.readFileSync('./conf/spicy.local/spicy.local-crt.pem', 'utf8')
+};
 
-// A server that makes an operation that waits a while and then proxies the request
-http
-  .createServer((req, res) => {
-    console.log(`${req.method} ${req.url}`);
+const proxy = httpProxy.createProxyServer({
+  ssl: sslOpts,
+  target: TARGET_URL,
+  secure: false
+});
 
-    // This simulates an operation that takes longer to execute
-    setTimeout(() => {
-      proxy.web(req, res, { target: TARGET_URL });
-    }, TIMEOUT_TIME);
-  })
-  .listen(LISTEN_PORT);
+const proxyServer = https.createServer(sslOpts, (req, res) => {
+  console.log(`${req.method} ${req.url}`);
+
+  setTimeout(() => {
+    proxy.web(req, res, { target: TARGET_URL });
+  }, TIMEOUT_TIME);
+});
+
+proxyServer.listen(LISTEN_PORT);
 
 console.log(`Listening on ${LISTEN_PORT}, proxying to ${TARGET_URL}`);
